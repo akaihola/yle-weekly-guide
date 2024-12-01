ed8546e0a2db650afe20c04aaec80899cb4a8676
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2024-12-01 21:40:41 +0200

    style: Add stylelint configuration and CSS formatting scripts

--- run_lint.sh
+++ run_lint.sh
@@ -30,4 +30,17 @@ if [ ${#js_files[@]} -gt 0 ]; then
     npm run format:js -- "${js_files[@]}" || errors=$?
 fi
 
+# Check for CSS files
+css_files=()
+for file in "$@"; do
+    if [[ $file == *.css ]]; then
+        css_files+=("$file")
+    fi
+done
+
+if [ ${#css_files[@]} -gt 0 ]; then
+    npm run lint:css -- "${css_files[@]}" || errors=$?
+    npm run format:css -- "${css_files[@]}" || errors=$?
+fi
+
 exit $errors
